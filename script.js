2cc480ad7600d833ae720e58ea96f5ee7655fb44
Alex <Alex@aaa.com>
2023-08-16 10:11:19 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,37 @@
+var leftBorderRight = 1;
+let second = 2;
+const pi = 3.14;
+
+let persone = {
+    name: "John",
+    age: 25,
+    isMarried: false
+}
+console.log(persone["age"])
+
+let arr = ['plum.png', 'orange.jpg'];
+
+console.log(arr[1]);
+
+console.log(typeof(
+    String(null))); 
+
+
+console.log("ww" + "er");
+
+
+// console.log()
+
+console.log(typeof(+'g'));
+
+console.log(+'g');
+
+console.log((parseInt('15px', 10)))
+
+
+let incr = 10,
+    decr = 15;
+console.log(incr++);
+console.log(incr);
+
+console.log(3 === 2);
